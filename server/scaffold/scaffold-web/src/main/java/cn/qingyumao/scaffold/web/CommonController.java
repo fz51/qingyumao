ef5ba2cad133fbfe3ee3755eae842d2a56c86afd
qingyumao <fz_5151@163.com>
2023-03-16 22:44:41 +0800

    Daily submission

--- server/scaffold/scaffold-web/src/main/java/cn/qingyumao/scaffold/web/CommonController.java
+++ server/scaffold/scaffold-web/src/main/java/cn/qingyumao/scaffold/web/CommonController.java
@@ -1,13 +1,18 @@
 package cn.qingyumao.scaffold.web;
 
 import cn.qingyumao.scaffold.execution.CmdDispatcher;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.SmartInitializingSingleton;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.util.Assert;
 
-public class CommonController implements SmartInitializingSingleton {
+public class CommonController implements SmartInitializingSingleton, BeanFactoryPostProcessor {
+
+    private ConfigurableListableBeanFactory beanFactory;
 
     private CmdDispatcher cmdDispatcher;
+
 
     public void handleCmd(Object cmd) {
         cmdDispatcher.dispatch(cmd);
@@ -15,11 +20,12 @@ public class CommonController implements SmartInitializingSingleton {
 
     @Override
     public void afterSingletonsInstantiated() {
+        this.cmdDispatcher = beanFactory.getBean(CmdDispatcher.class);
         Assert.notNull(cmdDispatcher, "cmdDispatcher is not null");
     }
 
-
-    public void setCmdDispatcher(@Autowired CmdDispatcher cmdDispatcher) {
-        this.cmdDispatcher = cmdDispatcher;
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
     }
 }
